f8652fa1cd0f95895f3076eff5aa31b01eadfca0
Baileyton <baileyton@naver.com>
2024-02-28 17:57:47 +0900

    first commit

---
+++ src/main/java/com/example/camping/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.example.camping.controller;
+
+import groovy.util.logging.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class MainController {
+
+    @GetMapping(value = "/")
+    public String home() {
+        return "main";
+    }
+}
